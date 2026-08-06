1d83870b0b6ef4c51844e8b319b567c0dd77383b
none_None1 <mxm20110612@163.com>
2026-08-06 22:41:50 +0800

    Add FizzBuzz example

---
+++ examples/fizz_buzz.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	for(int i=1;i<=100;i++){
+		if(i%5){
+			if(i%3) printf("%d\n",i);
+			else puts("Fizz");
+		}else{
+			if(i%3) puts("Buzz");
+			else puts("FizzBuzz");
+		}
+	}
+	return 0;
+}
